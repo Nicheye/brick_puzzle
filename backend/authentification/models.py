d2bafdb70cc91be475fe72ec840aa0f767ea403e
Michel Shpilevsky <68427995+Nicheye@users.noreply.github.com>
2024-08-11 20:38:31 +0300

    deploy fix #23

--- backend/authentification/models.py
+++ backend/authentification/models.py
@@ -34,7 +34,7 @@ class CustomUserManager(BaseUserManager):
 class User(AbstractUser):
     username = None
     email = models.EmailField(max_length=255, unique=True)
-    can_add = models.BooleanField(default=False)
+    can_add = models.BooleanField(default=True)
     USERNAME_FIELD = 'email'
     REQUIRED_FIELDS = []
 
